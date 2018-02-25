5a193daf5f3fddb80620cac6c045206249686756
surdu <postal2600@gmail.com>
2018-02-25 23:07:35 +0200

    Spamming return key will not render you deaf anymore. Fixes #2

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,7 @@
-var path = require("path");
-var pressed = {};
+const path = require("path");
+const pressed = {};
+
+const returnKeyDownSound = new Audio(path.join(__dirname, "audio/return.mp3"));
 
 function handleKeyDown(event) {
 	if (!pressed[event.keyCode]) {
@@ -9,23 +11,24 @@ function handleKeyDown(event) {
 		key_down.play();
 
 		if (event.keyCode === 13) {
-			var key_down = new Audio(path.join(__dirname, "audio/return.mp3"));
-			key_down.play();
+			returnKeyDownSound.pause();
+			returnKeyDownSound.currentTime = 0.
+			returnKeyDownSound.play();
 		}
 	}
 }
 
 function handleKeyUp(event) {
-		pressed[event.keyCode] = false;
+	pressed[event.keyCode] = false;
 
-		var key_up = new Audio(path.join(__dirname, "audio/key_up.mp3"));
-		key_up.play();
+	const key_up = new Audio(path.join(__dirname, "audio/key_up.mp3"));
+	key_up.play();
 }
 
 module.exports = {
 	activate: function(state) {
 		atom.workspace.observeTextEditors(function(editor) {
-			var editorView = atom.views.getView(editor);
+			const editorView = atom.views.getView(editor);
 
 			editorView.addEventListener('keydown', handleKeyDown);
 			editorView.addEventListener('keyup', handleKeyUp);
@@ -34,7 +37,7 @@ module.exports = {
 
 	deactivate: function () {
 		atom.workspace.observeTextEditors(function(editor) {
-			var editorView = atom.views.getView(editor);
+			const editorView = atom.views.getView(editor);
 
 			editorView.removeEventListener('keydown', handleKeyDown);
 			editorView.removeEventListener('keyup', handleKeyUp);
